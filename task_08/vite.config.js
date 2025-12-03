11af7bc0d2468c58b01caccd410c113ae9d8a690
Alex07 <yarmolaaleksandr@gmail.com>
2025-12-03 17:10:50 +0300

    edit task_08

--- task_08/vite.config.js
+++ task_08/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/all_tasks_v23/task_08/',
+  base: './',
   plugins: [react()],
   test: {
     globals: true,
